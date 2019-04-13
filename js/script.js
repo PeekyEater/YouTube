849d09b95580155e8d57a6bbc55d0229cb7ff597
Vlad <vlad.mangoman@gmail.com>
2019-04-13 21:24:02 +0300

    Added YT API interaction: videos request & play. And buttons More and Less added.

--- js/script.js
+++ js/script.js
@@ -56,7 +56,135 @@ function switchMode() {
     }
 }
 
+function sliceTitle(selector, count) {
+    document.querySelectorAll(selector).forEach(item => {
+        item.textContent.trim();
+        if (item.textContent.length < count) {
+            return;
+        } else {
+            const str = item.textContent.slice(0, count + 1) + '...';
+            item.textContent = str;
+        }
+    });
+}
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+    player.stopVideo();
+}
+
+function bindModal(cards) {
+    cards.forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+            const id = item.getAttribute('data-url');
+            loadVideo(id);
+            openModal();
+        });
+    });
+}
+
+function createVideo() {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+   setTimeout(() => {
+    player = new YT.Player('frame', {
+        height: '100%',
+        width: '100%',
+        videoId: 'M7lc1UVf-VE',
+      });
+   }, 300);
+
+}
+
+function loadVideo(id) {
+    player.loadVideoById({'videoId': `${id}`});
+}
+
+function start() {
+    gapi.client.init({
+        'apiKey': 'AIzaSyB03ygVmuojHox1yYAZUz8l2FZA9gwt7TI',
+        'discoveryDocs': ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"]
+    }).then(function(){
+        return gapi.client.youtube.playlistItems.list({
+            "part": "snippet,contentDetails",
+            "maxResults": '9',
+            "playlistId": "PLnOieOP_Tjow38LqU7FfgvSQ5_g5qnnWA" 
+        });
+    }).then(function(response){
+        const videosWrapper = document.querySelector('.videos__wrapper');
+        response.result.items.forEach(item => {
+            let card = document.createElement('a');
+            card.classList.add('videos__item', 'videos__item-active');
+            card.setAttribute('data-url', item.contentDetails.videoId);
+            card.innerHTML = `
+                <img src="${item.snippet.thumbnails.high.url}" alt="thumb">
+                <div class="videos__item-descr">
+                    ${item.snippet.title}
+                </div>
+                <div class="videos__item-views">
+                    2.7 тыс. просмотров?
+                </div>
+            `;
+            videosWrapper.appendChild(card);
+            setTimeout(() => {
+                card.classList.remove('videos__item-active');}, 100);
+            if(document.body.classList.contains('night')) {
+                card.querySelector('.videos__item-descr').style.color = '#fff';
+                card.querySelector('.videos__item-views').style.color = '#fff';
+            }
+        });
+        sliceTitle('.videos__item-descr', 100);
+        bindModal(document.querySelectorAll('.videos__item'));
+    }).catch(e => {
+        console.log(e);
+    });
+}
+
 bindSlideToggle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-active');
+
 switcher.addEventListener('change', () => {
     switchMode();
 });
+
+modal.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('modal__body')) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode === 27) {
+        closeModal();
+    }
+});
+
+more.addEventListener('click', () => {
+    let less = document.createElement('button');
+    less.classList.add('less');
+    less.innerHTML = 'Show less';
+    document.querySelector('.videos').appendChild(less);
+    less.addEventListener('click', () => {
+        const videosWrapper = document.querySelector('.videos__wrapper');
+        document.querySelector('.less').remove();
+        while( videosWrapper.firstChild ) {
+            videosWrapper.removeChild(videosWrapper.firstChild);
+        }
+        setTimeout( () => {
+        more.style.display = 'block';
+        }, 0);
+    });
+    more.style.display = 'none';
+    setTimeout( () => {
+    less.style.opacity = '1';
+    }, 1000);
+    gapi.load('client', start);
+});
+
+createVideo();
